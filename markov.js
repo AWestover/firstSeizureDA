a384ebca3d38a4a93dfdddf761f0b472b204c5f1
Alek Westover <alek.westover@gmail.com>
2018-11-11 17:40:42 -0500

    graph 2

--- markov.js
+++ markov.js
@@ -1,54 +1,89 @@
 /* matrix */
 
+let models = ["model 1", "model 2"];
 // model parameters
 let pAE = 0.22; // risk of AED side affects
-let Efficacy = 0.75; // constant
+let Efficacy = {
+	"model 1": 0.75,
+	"model 2": 1.00
+};
 let baseCase = 1;
-let x = [1-pAE,0,pAE,0,0]; // initial probabilities
-let probabilities = [];
-let utilities = [];
 
 // must input base case for recurrence risks
-function transition_matrix(pt_data, t) {
+function transition_matrix(pt_data, t, model) {
 	let year = t+pt_data.age;
 	if (year >= muASR.length) {
 		year = muASR.length - 1;
 	}
+	let out;
+	if (model == 'model 1')
+	{
+		let da = 1-math.exp(-muASR[year]);
+		let temp = math.exp(muASR[year])-1;
+		let db = pt_data.MR*temp / (1+pt_data.MR*temp);
 
-	let da = 1-math.exp(-muASR[year]);
-	let temp = math.exp(muASR[year])-1;
-	let db = pt_data.MR*temp / (1+pt_data.MR*temp);
+		let d1  = da;
+		let a21 = (1-d1)*pt_data.recurrence_risk[year] * Efficacy[model];
+		let a11 = 1-d1-a21;
 
-	let d1  = da;
-	let a21 = pt_data.recurrence_risk[year] * Efficacy;
-	let a11 = 1-d1-a21;
+		let d2  = db;
+		let a22 = 1-d2;
 
-	let d2  = db;
-	let a22 = 1-d2;
+		let d3  = da;
+		let a43 = (1-d3)*pt_data.recurrence_risk[year] * Efficacy[model];
+		let a33 = 1-d3-a43;
 
-	let d3  = da;
-	let a43 = pt_data.recurrence_risk[year] * Efficacy;
-	let a33 = 1-d3-a43;
+		let d4  = db;
+		let a44 = 1-d4;
 
-	let d4  = db;
-	let a44 = 1-d4;
+		out = math.matrix([
+			[a11,0  ,0  ,0  ,0  ],
+			[a21,a22,0  ,0  ,0  ],
+			[0  ,0  ,a33,0  ,0  ],
+			[0  ,0  ,a43,a44,0  ],
+			[d1 ,d2 ,d3 ,d4 ,1  ]
+		]);
+	}
+	else if (model == 'model 2')
+	{
+		let da = 1-math.exp(-muASR[year]);
+		let temp = math.exp(muASR[year])-1;
+		let db = pt_data.MR*temp / (1+pt_data.MR*temp);
 
-	let x = math.matrix([
-		[a11,0  ,0  ,0  ,0  ],
-		[a21,a22,0  ,0  ,0  ],
-		[0  ,0  ,a33,0  ,0  ],
-		[0  ,0  ,a43,a44,0  ],
-		[d1 ,d2 ,d3 ,d4 ,1  ]
-	]);
-	return (x);
+		let d1  = da;
+		let a21 = (1-d1)*pt_data.recurrence_risk[year] * Efficacy[model];
+		let a11 = 1-d1-a21;
+
+		let d2  = db;
+		let a22 = 1-d2;
+
+		let d3  = da;
+		let a43 = (1-d3)*pt_data.recurrence_risk[year] * Efficacy[model];
+		let a33 = 1-d3-a43;
+
+		let d4  = db;
+		let a44 = 1-d4;
+
+		out = math.matrix([
+			[a11,0  ,0  ,0  ,0  ],
+			[a21*(1-pAE),a22,0  ,0  ,0  ],
+			[0  ,0  ,a33,0  ,0  ],
+			[a21*pAE  ,0  ,a43,a44,0  ],
+			[d1 ,d2 ,d3 ,d4 ,1  ]
+		]);
+	}
+	return (out);
 }
 
-function calculateUtility(state) {
-	let weights = [0.967, 0.757, 0.8647, 0.6757, 0];
+function calculateUtility(state, model) {
+	let weights = {
+		"model 1": [0.967, 0.757, 0.8647, 0.6757, 0],
+		"model 2": [1, 0.757, 0.8647, 0.6757, 0]
+	}
 	let utility = 0;
 	for (let i = 0; i < state.length; i++)
 	{
-		utility += state[i]*weights[i];
+		utility += state[i]*weights[model][i];
 	}
 	return utility;
 }
@@ -93,7 +128,7 @@ function runProcess() {
 				recRisk.push(0.01/100);
 		}
     }
-	else { //baseCase ==2 | baseCase==3; 
+	else { //baseCase ==2 | baseCase==3;
 	    for (let i = 0; i < 500; i++) {
 			if (i-age < 0)
 				recRisk.push(1);
@@ -113,63 +148,103 @@ function runProcess() {
 }
 
 function calculations(pt_data) {
-	for (let i = 1; i < 100; i++) {
-		probabilities.push(x);
-		x = math.multiply(transition_matrix(pt_data, i),x).toArray();
-		utilities.push(calculateUtility(x));
+	let x = { // initial probabilities
+		"model 1": [1-pAE,0,pAE,0,0],
+		"model 2":[1,0,0,0,0]
+	};
+	let probabilities = {
+		"model 1": [],
+		"model 2": []
+	};
+	let utilities = {
+		"model 1": [],
+		"model 2": []
+	};
+	for (let m in models)
+	{
+		for (let i = 1; i < 100; i++) {
+			probabilities[models[m]].push(x[models[m]]);
+			x[models[m]] = math.multiply(transition_matrix(pt_data, i, models[m]),x[models[m]]).toArray();
+			utilities[models[m]].push(calculateUtility(x[models[m]], models[m]));
+		}
 	}
 
-	let totalUtility = 0;
-	for (let i = 0; i < utilities.length; i++)
+	let totalUtility = {"model 1": 0, "model 2": 0};
+	for (let m in models) {
 	{
-		totalUtility += utilities[i];
+		for (let i = 0; i < utilities[models[m]].length; i++)
+			totalUtility[models[m]] += utilities[models[m]][i];
+		}
 	}
-	$("#totalUtility").text("Total Utility: "+totalUtility.toFixed(2));
+	console.log(totalUtility);
+	$("#totalUtility1").text("Total Utility: "+totalUtility["model 1"].toFixed(2));
+	$("#totalUtility2").text("Total Utility: "+totalUtility["model 2"].toFixed(2));
+
+	if(totalUtility["model 1"] > totalUtility["model 2"])
+	{
+		$("#recommended").text("recommended: model 1");
+	}
+	else {
+		$("#recommended").text("recommended: model 2");
+	}
 
 	let new_order = [4,3,1,2,0];
-	let curvesY = [];
-	for (let i = 0; i < probabilities[0].length; i++) {
-		curvesY.push([]);
-		if (i == 0)
-		{
-			for (let j = 0; j < probabilities.length; j++)
+	let curvesY = {"model 1": [], "model 2": []};
+	for (let m in models)
+	{
+		for (let i = 0; i < probabilities[models[m]][0].length; i++) {
+			curvesY[models[m]].push([]);
+			if (i == 0)
 			{
-				curvesY[0].push(probabilities[j][new_order[i]]);
+				for (let j = 0; j < probabilities[models[m]].length; j++)
+				{
+					curvesY[models[m]][0].push(probabilities[models[m]][j][new_order[i]]);
+				}
 			}
-		}
-		else {
-			for (let j = 0; j < probabilities.length; j++)
-			{
-				curvesY[i].push(probabilities[j][new_order[i]]+curvesY[i-1][j]);
+			else {
+				for (let j = 0; j < probabilities[models[m]].length; j++)
+				{
+					curvesY[models[m]][i].push(probabilities[models[m]][j][new_order[i]]+curvesY[models[m]][i-1][j]);
+				}
 			}
 		}
 	}
 
 	let colors = ['#08519c','#3182bd','#6baed6','#bdd7e7','#eff3ff']
-	let data = [];
-	for (let i = 0; i < curvesY.length; i++)
+	let data = {"model 1": [], "model 2": []};
+	for (let m in models)
 	{
-		let cTrace = {
-			y: curvesY[i],
-			mode: 'line',
-			fill: 'tonexty',
-			fillcolor: colors[i],
-			stroke: '#00000', //????
-			name: '#'+i
+		for (let i = 0; i < curvesY[models[m]].length; i++)
+		{
+			let cTrace = {
+				y: curvesY[models[m]][i],
+				mode: 'none',
+				fill: 'tonexty',
+				fillcolor: colors[i],
+				name: '#'+i
+			}
+			data[models[m]].push(cTrace);
 		}
-		data.push(cTrace);
 	}
 
 	let layout = {
-		title: 'prob', 
+		title: 'probabilities',
 		xaxis: {
 			title: 'Years'
 		},
 		yaxis: {
 			title: 'probabilities'
+		},
+		margin: {
+			l: 50,
+			r: 30,
+			b: 30,
+			t: 30
 		}
 	};
 
-	Plotly.newPlot('div1', data, layout);
+	Plotly.newPlot('graph1', data["model 1"], layout);
+	Plotly.newPlot('graph2', data["model 2"], layout);
 }
 
+runProcess();
